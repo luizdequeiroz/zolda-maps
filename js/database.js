20f3c0dcc18c1c932eebdbd6ea940c20d75468f5
luizdequeiroz <luizdequeiroz@outlook.com.br>
2016-07-19 10:24:47 -0300

    commit inicial

---
+++ js/database.js
@@ -0,0 +1,18 @@
+﻿var subs = [
+    { name: 'cinzenta', lat: 1.626, lng: -68.159, zoom: 6 },
+    { name: 'pedras', lat: 13.923, lng: -69.368, zoom: 6 },
+    { name: 'maraba', lat: 25.780, lng: -67.412, zoom: 6 },
+    { name: 'fieldon', lat: 13.966, lng: -45.396, zoom: 6 },
+    { name: 'zionr', lat: 25.404, lng: -52.976, zoom: 6 },
+    { name: 'matim', lat: 25.443, lng: -37.595, zoom: 6 },
+    { name: 'confins', lat: 25.125, lng: -26.038, zoom: 6 },
+    { name: 'sagmatim', lat: 24.607, lng: -17.336, zoom: 6 },
+    { name: 'niogro', lat: 16.784, lng: -4.504, zoom: 6 },
+    { name: 'mileni', lat: 5.157, lng: -0.066, zoom: 6 }
+];
+
+function getSubMap(name) {
+    return subs.filter(function (obj) {
+        return obj.name === name;
+    })[0];
+}
---
+++ js/map.js
@@ -0,0 +1,276 @@
+var overlay;
+
+var marker;
+var Map;
+var mapMarkers = new Array();
+
+var rectangle = new google.maps.Rectangle();
+var polyGon = new google.maps.Polygon();
+var poly = new google.maps.Polyline();
+var outlineMarkers = new Array();
+
+USGSOverlay.prototype = new google.maps.OverlayView();
+
+var submap;
+
+function initMap() {
+
+    var styles = [
+      {
+          stylers: [
+            { visibility: "off" }
+          ]
+      }
+    ];
+
+    var styledMap = new google.maps.StyledMapType(styles, { name: "Styled Map" });
+
+    Map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 5,
+        minZoom: 4,
+        maxZoom: 8,
+        center: new google.maps.LatLng(14, -35),
+        mapTypeControl: false,
+        streetViewControl: false,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        }
+    });
+
+    Map.mapTypes.set('map_style', styledMap);
+    Map.setMapTypeId('map_style');
+
+    var bounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(-5.134475371454281, -79.50845859491564),
+        new google.maps.LatLng(30.67883861843332, 6.762828376789505));
+
+    var srcImage = 'img/OMundoConhecido.png';
+
+    poly = new google.maps.Polyline({
+        map: Map,
+        path: [],
+        strokeColor: 'darkblue',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+
+    polyGon = new google.maps.Polygon({
+        map: Map,
+        path: [],
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+        draggable: true
+    });
+
+    google.maps.event.addListener(polyGon, "drag", function (dragEvent) {
+        var polyGonPoints = this.getPath().getArray();
+        $.each(polyGonPoints, function (index, val) {
+            var polyGonPoint = val;
+            outlineMarkers[index].setPosition(polyGonPoint);
+        });
+    });
+
+    google.maps.event.addListener(Map, "rightclick", function (event) {
+        //event.preventDefault();
+
+        var markerIndex = poly.getPath().length;
+        poly.setMap(Map);
+        var isFirstMarker = markerIndex === 0;
+        var marker = new google.maps.Marker({
+            map: Map,
+            position: event.latLng,
+            draggable: true
+        });
+
+        google.maps.event.addListener(marker, 'rightclick', function () {
+            marker.setMap(null);
+        });
+
+        if (isFirstMarker) {
+            google.maps.event.addListener(marker, 'click', function () {
+                var path = poly.getPath();
+                polyGon.setPath(path);
+                polyGon.setMap(Map);
+
+                google.maps.event.addListener(polyGon, 'rightclick', function () {
+                    polyGon.setMap(null);
+                });
+            });
+            marker.setIcon('img/firstIcon.png');
+        } else {
+            marker.setIcon('img/secondsIcons.png');
+        }
+        
+        poly.getPath().push(event.latLng);
+
+        outlineMarkers.push(marker);
+
+        google.maps.event.addListener(marker, 'drag', function (dragEvent) {
+            poly.getPath().setAt(markerIndex, dragEvent.latLng);
+            updateDistance(outlineMarkers);
+        });
+
+        updateDistance(outlineMarkers);
+    });
+
+    overlay = new USGSOverlay(bounds, srcImage, Map);
+
+    document.getElementById('refresh').addEventListener('click', function () {
+        outlineMarkers = new Array();
+        initMap();
+    });
+
+    Map.addListener('mouseover', function () {
+        Map.addListener('mousemove', function (e) {
+            $('#coordenadas').val('Lat ' + e.latLng.lat().toFixed(3) + ' | Lng ' + e.latLng.lng().toFixed(3));
+        });
+
+        Map.addListener('mouseout', function (e) {
+            //mostrar coordenadas
+        });
+    });
+
+    var drawingManager = new google.maps.drawing.DrawingManager({
+        drawingControl: true,
+        drawingControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.RIGHT_BOTTOM,
+            drawingModes: [
+              google.maps.drawing.OverlayType.MARKER,
+              google.maps.drawing.OverlayType.CIRCLE,
+              google.maps.drawing.OverlayType.POLYGON,
+              google.maps.drawing.OverlayType.POLYLINE,
+              google.maps.drawing.OverlayType.RECTANGLE
+            ]
+        },
+        markerOptions: { icon: 'img/firstIcon.png' },
+        circleOptions: {
+            fillColor: '#ffff00',
+            fillOpacity: 1,
+            strokeWeight: 5,
+            clickable: false,
+            editable: true,
+            zIndex: 1
+        }
+    });
+    drawingManager.setMap(Map);
+}
+
+function updateDistance(outlineMarkers) {
+    var totalDistance = 0.0;
+    var last = undefined;
+    $.each(outlineMarkers, function (index, val) {
+        if (last) {
+            var dist = google.maps.geometry.spherical.computeDistanceBetween(last.position, val.position) / 1000;
+            console.log('Adicional: ' + parseFloat(dist.toFixed(3)));
+            totalDistance += google.maps.geometry.spherical.computeDistanceBetween(last.position, val.position) / 1000;
+        }
+        last = val;
+    });
+    console.log('Total: ' + parseFloat(totalDistance.toFixed(3)))
+    $('#distancia').val(parseFloat(totalDistance.toFixed(3)) + ' Km');
+}
+
+function USGSOverlay(bounds, image, map) {
+
+    this.bounds_ = bounds;
+    this.image_ = image;
+    this.map_ = map;
+
+    this.div_ = null;
+
+    this.setMap(map);
+}
+
+USGSOverlay.prototype.onAdd = function () {
+
+    var div = document.createElement('div');
+    div.style.borderStyle = 'none';
+    div.style.borderWidth = '0px';
+    div.style.position = 'absolute';
+
+    var img = document.createElement('img');
+    img.src = this.image_;
+    img.style.width = '100%';
+    img.style.height = '100%';
+    img.style.position = 'absolute';
+    div.appendChild(img);
+
+    this.div_ = div;
+
+    var panes = this.getPanes();
+    panes.overlayLayer.appendChild(div);
+};
+
+USGSOverlay.prototype.draw = function () {
+
+    var overlayProjection = this.getProjection();
+
+    var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+    var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+    var div = this.div_;
+    div.style.left = sw.x + 'px';
+    div.style.top = ne.y + 'px';
+    div.style.width = (ne.x - sw.x) + 'px';
+    div.style.height = (sw.y - ne.y) + 'px';
+};
+
+USGSOverlay.prototype.onRemove = function () {
+    this.div_.parentNode.removeChild(this.div_);
+    this.div_ = null;
+};
+
+google.maps.event.addDomListener(window, 'load', initMap);
+
+function calcTime() {
+    var vel = parseFloat($('#modo').val());
+
+    if (vel == 0) return;
+
+    var dis = parseFloat($('#distancia').val());
+
+    var tem = (dis / vel) + 0.02;
+    var dia = 0;
+    var hor = parseInt(tem);
+    var min = parseInt((tem - hor) * 60);
+
+    if (hor % 24 == 0) dia = hor / 24;
+    else dia = parseInt(hor / 24);
+
+    hor = hor - (24 * dia);
+    
+    var result = "";
+    if (dia > 0)
+        if (dia > 1) result += dia + ' dias ';
+        else result += dia + ' dia ';
+    if ((hor > 0 || min > 0) && dia > 0) result += 'e ';
+    if (hor > 0)
+        if (hor > 1) result += hor + 'hs ';
+        else result += hor + 'h ';
+    if (min > 1)
+        if (min < 10) result += '0' + min + 'min';
+        else result += min + 'min';
+
+    if (vel % 2 != 0) {
+        switch(vel){
+            case 4.17: $('#tempo').attr('title', 'descansando 1h e 30min a cada 50km'); break;
+            case 7.14: $('#tempo').attr('title', 'descansando 2hs a cada 50km'); break;
+            case 11.11: $('#tempo').attr('title', 'descansando 30min a cada 50km'); break;
+            case 33.33: $('#tempo').attr('title', 'descansando 30min a cada 50km'); break;
+        }
+    }
+
+    $('#tempo').val(result);
+}
+
+function subMap(){
+    var name = $('#pesquisa').val();
+    var submap = getSubMap(name);
+    console.log('name: ' + submap.name + ' | lat: ' + submap.lat + ' | lng: ' + submap.lng + ' | zoom: ' + submap.zoom);
+    Map.panTo(new google.maps.LatLng(submap.lat, submap.lng));
+    Map.setZoom(submap.zoom);
+}
